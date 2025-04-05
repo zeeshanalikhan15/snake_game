5bf347a4704fcc51091c8b7ae43c03b96a22bd54
zeeshanalikhan15 <zeeshanalikhan15@gmail.com>
2025-04-05 08:57:22 +0200

    snake tongue

--- snake.rb
+++ snake.rb
@@ -16,6 +16,9 @@ $food = [
 $score = 0
 $game_over = false
 
+# Add a variable to track tongue visibility
+$tongue_visible = false
+
 # Handle keyboard input
 on :key_down do |event|
   case event.key
@@ -84,13 +87,59 @@ update do
     )
 
     # Draw snake
-    $snake.each do |x, y|
-      Square.new(
-        x: x, y: y,
-        size: CELL_SIZE,
-        color: 'green'
-      )
+    $snake.each_with_index do |(x, y), index|
+      if index.zero?
+        # Draw the snake's head
+        Square.new(
+          x: x, y: y,
+          size: CELL_SIZE,
+          color: 'green' # Replaced 'darkgreen' with 'green'
+        )
+        # Draw the tongue if visible
+        if $tongue_visible
+          case $direction
+          when 'up'
+            Line.new(
+              x1: x + CELL_SIZE / 2, y1: y,
+              x2: x + CELL_SIZE / 2, y2: y - CELL_SIZE / 2,
+              width: 2,
+              color: 'red'
+            )
+          when 'down'
+            Line.new(
+              x1: x + CELL_SIZE / 2, y1: y + CELL_SIZE,
+              x2: x + CELL_SIZE / 2, y2: y + CELL_SIZE + CELL_SIZE / 2,
+              width: 2,
+              color: 'red'
+            )
+          when 'left'
+            Line.new(
+              x1: x, y1: y + CELL_SIZE / 2,
+              x2: x - CELL_SIZE / 2, y2: y + CELL_SIZE / 2,
+              width: 2,
+              color: 'red'
+            )
+          when 'right'
+            Line.new(
+              x1: x + CELL_SIZE, y1: y + CELL_SIZE / 2,
+              x2: x + CELL_SIZE + CELL_SIZE / 2, y2: y + CELL_SIZE / 2,
+              width: 2,
+              color: 'red'
+            )
+          end
+        end
+      else
+        # Draw the rest of the snake's body
+        Square.new(
+          x: x, y: y,
+          size: CELL_SIZE,
+          color: 'green'
+        )
+      end
     end
+
+    # Toggle tongue visibility every 10 frames
+    $tongue_visible = !$tongue_visible if (Window.frames % 10).zero?
 
     # Move snake every 150ms
     if (Window.frames % 25).zero?
